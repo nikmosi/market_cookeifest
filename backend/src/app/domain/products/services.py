ee7ca38b22b9397f72881b15ddcd8017f55d2f2d
nikmosi <i@nikmosi.ru>
2024-11-29 23:01:38 +0700

    create services for Product

--- backend/src/app/domain/products/services.py
+++ backend/src/app/domain/products/services.py
@@ -1,8 +1,10 @@
-import json
+from datetime import date
 
 import requests
+from pydantic import HttpUrl
 
 from app.db.models import Article, Catalog
+from app.db.models.catalog import Product
 
 
 def get_product_data_by_article(article: str) -> Article:
@@ -20,3 +22,36 @@ def get_products_by_query(query: str) -> Catalog:
             f"https://search.wb.ru/exactmatch/ru/common/v7/search?ab_testing=false&appType=1&curr=rub&dest=-366541&query={query}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false"
         ).text
     )
+
+
+def generate_optimal_query(article: Article) -> str:
+    return article.imt_name
+
+
+def sort_products_by_ollama(products: list[Product]) -> list[int]:
+    # можно вернуть отсортированные продукты list[Product], но в этом особого смысла нет
+    articles = [p.id for p in products]
+    return articles
+
+
+def calculate_price(article: Article) -> float:
+    return 99.99
+
+
+def calculate_delivery_date(article: Article) -> date:
+    return date.today()
+
+
+def calculate_rating(article: Article) -> float:
+    return 4.5
+
+
+def calculate_reviews_count(article: Article) -> int:
+    return 100
+
+
+def get_image_urls(article: Article) -> list[HttpUrl]:
+    return [
+        HttpUrl(f"https://example.com/image_{i}.jpg")
+        for i in range(article.media.photo_count)
+    ]
