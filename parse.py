0097dfcfa1d88d2d0e3569135559101edc64caf1
idDeqpt <166838055+idDeqpt@users.noreply.github.com>
2024-11-29 16:16:49 +0700

    Add files via upload

--- parse.py
+++ parse.py
@@ -2,28 +2,17 @@ import requests
 import json
 
 
-#def utf2web(text: str):
-    
+def getProductDataByArticle(article: str):
+    length = len(article)
+    return json.loads(requests.get(f"https://basket-{('0' + article[ : 1]) if (length == 8) else article[ : 2]}.wbbasket.ru/vol{article[ : 3] if (length == 8) else article[ : 5]}/part{article[ : 5] if (length == 8) else article[ : 6]}/{article}/info/ru/card.json").text)
+
+def getProductsByQuery(query: str):
+    return json.loads(requests.get(f"https://search.wb.ru/exactmatch/ru/common/v7/search?ab_testing=false&appType=1&curr=rub&dest=-366541&query={query}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false").text)
     
 
 if (__name__ == "__main__"):
-    
-    
     query = "фен"#input("text: ")
     art = "48334378"
-    #url = f"https://search.wb.ru/exactmatch/ru/common/v7/search?ab_testing=false&appType=1&curr=rub&dest=-366541&query={query}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false"
-    url = f"https://basket-{('0' + art[ : 1]) if (len(art) == 8) else art[ : 2]}.wbbasket.ru/vol{art[ : 3] if (len(art) == 8) else art[ : 5]}/part{art[ : 5] if (len(art) == 8) else art[ : 6]}/{art}/info/ru/card.json"
-    print(url)
-    req = requests.get(url)
-    print(req.text)
-    src = json.loads(req.text)
-    print(src)
-    
-    
-    
-    products_list = list(src["data"]["products"])
-    for pr in products_list:
-        print(pr)
-    print(list(src["data"]["products"]))
-
+    print(getProductDataByArticle("48334378"))
+    print(getProductsByQuery(query))
         
