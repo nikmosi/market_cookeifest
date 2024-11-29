a9b42214b86a9c03fb52f3553d4c4f9e7f50dbbe
nikmosi <i@nikmosi.ru>
2024-11-29 20:43:02 +0700

    add get_product_by_article

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,6 +1,9 @@
+from typing import Any
+
 from litestar import Litestar, get
+
 from app.models import WbAns
-from app.parse import getProductsByQuery
+from app.parse import getProductDataByArticle, getProductsByQuery
 
 
 @get("/")
@@ -13,9 +16,14 @@ async def get_catalog_by_query(text: str) -> WbAns:
     return getProductsByQuery(text)
 
 
+@get("/catalog/{article:str}")
+async def get_product_by_article(article: str) -> Any:
+    return getProductDataByArticle(article)
+
+
 def create_app() -> Litestar:
     return Litestar(
-        [index, get_catalog_by_query],
+        [index, get_catalog_by_query, get_product_by_article],
     )
 
 
