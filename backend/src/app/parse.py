6d3c795b3ed9bace90bbd46b0f74f921484e13c3
idDeqpt <166838055+idDeqpt@users.noreply.github.com>
2024-11-30 12:09:51 +0700

    multiprocess added

--- backend/src/app/parse.py
+++ backend/src/app/parse.py
@@ -2,8 +2,25 @@ import requests
 import json
 import time
 from datetime import datetime
+import os
+import threading
 
-from app.db.models import Catalog
+from models import WbAns
+
+
+def setValueInList(l, i, v):
+    l[i] = v
+
+def multithreadExec(funcs: list):
+    results = [None]*len(funcs)
+    threads = []
+    for i in range(len(funcs)):
+        threads.append(threading.Thread(target = lambda: setValueInList(results, i, funcs[i][0](*funcs[i][1]))))
+        threads[-1].start()
+    
+    for thread in threads:
+        thread.join()
+    return results
 
 
 def getGeoData(latitude: str, longitude: str):
@@ -24,26 +41,33 @@ def getProductDelivery(article: str, dest: str):
 
 def getProductDataByArticle(article: str):
     length = len(article)
-    full_article = "0" * (9 - length) + article
-    print(article, full_article)
-    for i in range(1, 21):
-        req = requests.get(
-            f"https://basket-{('0' + str(i)) if (i < 10) else str(i)}.wbbasket.ru/vol{str(int(full_article[ : 4]))}/part{str(int(full_article[ : 6]))}/{article}/info/ru/card.json"
-        ).text
-        if req.find("DOCTYPE") == -1:
-            return json.loads(req)
+    full_article = "0"*(9 - length) + article
+    
+    results = []
+    threads_count = os.cpu_count()
+    steps = [threads_count for i in range(20//threads_count)]
+    steps.append(20%threads_count)
+    for step in steps:
+        results.extend(multithreadExec([[requests.get, [f"https://basket-{('0' + str(i)) if (i < 10) else str(i)}.wbbasket.ru/vol{str(int(full_article[ : 4]))}/part{str(int(full_article[ : 6]))}/{article}/info/ru/card.json"]] for i in range(1 + len(results), 1 + len(results) + step)]))
+    for result in results:
+        if (result.status_code == 200):
+            return json.loads(result.text)
     return None
 
 
 def findTitleImageUrlByArticle(article: str):
     length = len(article)
-    full_article = "0" * (9 - length) + article
-    for i in range(1, 21):
-        req = requests.get(
-            f"https://basket-{('0' + str(i)) if (i < 10) else str(i)}.wbbasket.ru/vol{str(int(full_article[ : 4]))}/part{str(int(full_article[ : 6]))}/{article}/images/big/1.webp"
-        )
-        if req.text.find("DOCTYPE") == -1:
-            return req.url
+    full_article = "0"*(9 - length) + article
+    
+    results = []
+    threads_count = os.cpu_count()
+    steps = [threads_count for i in range(20//threads_count)]
+    steps.append(20%threads_count)
+    for step in steps:
+        results.extend(multithreadExec([[requests.get, [f"https://basket-{('0' + str(i)) if (i < 10) else str(i)}.wbbasket.ru/vol{str(int(full_article[ : 4]))}/part{str(int(full_article[ : 6]))}/{article}/images/big/1.webp"]] for i in range(1 + len(results), 1 + len(results) + step)]))
+    for result in results:
+        if (result.status_code == 200):
+            return result.url
     return None
 
 
@@ -56,51 +80,54 @@ def getProductsByQuery_json(query: str):
 
 
 def getProductData(article: str, latitude: str, longitude: str):
-    product_data = getProductDataByArticle(article)
-    product_in_search = getProductsByQuery_json("товар " + article)["data"]["products"][
-        0
-    ]
-
-    geo_data = getGeoData(latitude, longitude)
+    data = multithreadExec([
+        [getProductDataByArticle, [article]],
+        [getProductsByQuery_json, ["товар " + article]],
+        [getGeoData, [latitude, longitude]],
+        [findTitleImageUrlByArticle, [article]]
+    ])
+    
+    product_data = data[0]
+    product_in_search = data[1]["data"]["products"][0]
+    
+    geo_data = data[2]
     dest_i = geo_data["xinfo"].find("dest=") + 5
     nearest_dest = geo_data["xinfo"][dest_i : geo_data["xinfo"].find("&", dest_i)]
-
-    product_delivery_hours = getProductDelivery(article, nearest_dest)["data"][
-        "products"
-    ][0]["time2"]
+    
+    product_delivery_hours = getProductDelivery(article, nearest_dest)["data"]["products"][0]["time2"]
     return [
         {
             "id": article,
             "name": product_data["imt_name"],
             "description": product_data["description"],
-            "price": float(product_in_search["sizes"][0]["price"]["total"] / 100),
-            "delivery": datetime.fromtimestamp(
-                time.time() + product_delivery_hours * 60 * 60
-            ).strftime("%d.%m.%Y"),
+            "price": float(product_in_search["sizes"][0]["price"]["total"]/100),
+            "delivery": datetime.fromtimestamp(time.time() + product_delivery_hours*60*60).strftime("%d.%m.%Y"),
             "rating": float(product_in_search["reviewRating"]),
             "reviews_count": int(product_in_search["feedbacks"]),
-            "options": dict(
-                [
-                    [option["name"], option["value"]]
-                    for option in product_data["options"]
-                ]
-            ),
+            "options": dict([[option["name"], option["value"]] for option in product_data["options"]])
         },
-        {"images": [findTitleImageUrlByArticle(article)]},
+        {"images": [data[3]]}
     ]
 
 
 def getFormatedProductsByQuery(query: str, latitude: str, longitude: str, count: int):
-    products_data = getProductsByQuery_json(query)["data"]["products"][:count]
+    products_data = getProductsByQuery_json(query)["data"]["products"]
     products = []
-    for product in products_data:
-        print(product)
-        products.append(getProductData(str(product["id"]), latitude, longitude))
+    threads_count = os.cpu_count()
+    steps = [threads_count for i in range(len(products_data)//threads_count)]
+    steps.append(len(products_data)%threads_count)
+    for step in steps:
+        products.extend(multithreadExec([[getProductData, [str(products_data[j]["id"]), latitude, longitude]] for j in range(len(products), len(products) + step)]))
     return products
 
 
-def getProductsByQuery(query: str) -> Catalog:
-    return Catalog.model_validate_json(
+def getProductsArticlesByQuery(query: str, latitude: str, longitude: str, count: int):
+    products_data = getProductsByQuery_json(query)["data"]["products"]
+    return [product["id"] for product in products_data]
+
+
+def getProductsByQuery(query: str) -> WbAns:
+    return WbAns.model_validate_json(
         requests.get(
             f"https://search.wb.ru/exactmatch/ru/common/v7/search?ab_testing=false&appType=1&curr=rub&dest=-366541&query={query}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false"
         ).text
@@ -108,11 +135,22 @@ def getProductsByQuery(query: str) -> Catalog:
 
 
 if __name__ == "__main__":
+    print("start")
     query = "фен"  # input("text: ")
     art = "48334378"
-    # print(getGeoData("55.309228", "82.730587"))
-    # print(getProductDelivery(art, "-7165699"))
-    print(getProductDataByArticle("3642034"))
-    # print(getFormatedProductsByQuery(query, "55.309228", "82.730587", 10))
-    # print(getProductData("149100663", "55.309228", "82.730587"))
+    #print(getGeoData("55.309228", "82.730587"))
+    #print(getProductDelivery(art, "-7165699"))
+    #print(getProductDataByArticle("149100663"))
+    #print(type(getProductDataByArticle__old("149100663")))
+    #print(getFormatedProductsByQuery(query, "55.309228", "82.730587", 10))
+    #print(getFormatedProductsByQueryTh(query, "55.309228", "82.730587"))
+    #print(getProductData("149100663", "55.309228", "82.730587"))
+    
+    curr_time = time.time()
+    #print(getFormatedProductsByQuery(query, "55.309228", "82.730587", 20))
+    print(time.time() - curr_time)
+    curr_time = time.time()
+    print(getProductsArticlesByQuery(query, "55.309228", "82.730587", 10))
+    print(time.time() - curr_time)
+    
     print("end")
