3cc47db3a212f55f7b3e9129b33583233da303db
nikmosi <i@nikmosi.ru>
2024-11-30 15:57:29 +0900

    add redis

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,4 +1,6 @@
 from litestar import Litestar, Request, get
+from litestar.config.response_cache import ResponseCacheConfig
+from litestar.stores.redis import RedisStore
 
 from app.routes import route_handlers
 
@@ -15,8 +17,14 @@ async def get_ip(request: Request) -> str:
 
 
 def create_app() -> Litestar:
+    redis_store = RedisStore.with_client(
+        url="redis://redis/", port=6379, db=0, username="default", password="password"
+    )
+    cache_config = ResponseCacheConfig(store="redis_backed_store")
     return Litestar(
         route_handlers=route_handlers + [index, get_ip],
+        stores={"redis_backed_store": redis_store},
+        response_cache_config=cache_config,
     )
 
 
