fe3f9561f1389d3127681b808bb26eb94d662461
CycHuk <DimaLanger@outlook.com>
2024-12-01 02:38:51 +0700

    fix axios timeout

--- frontend/src/API/api.jsx
+++ frontend/src/API/api.jsx
@@ -5,36 +5,37 @@ const host = import.meta.env.VITE_APP_HOST;
 console.log(host);
 
 const api = axios.create({
-	baseURL: host ? host : "",
-	headers: {
-		"Content-Type": "application/json",
-	},
+  baseURL: host ? host : "",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 60000,
 });
 
 export const fetchProducts = async (article) => {
-	try {
-		const response = await api.get(`/api/products/${article}`);
-		return response.data;
-	} catch (error) {
-		console.error("Error fetching products:", error);
-	}
+  try {
+    const response = await api.get(`/api/products/${article}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
 };
 
 export const fetchArticles = async (article) => {
-	try {
-		const response = await api.get(`/api/products/${article}/similar`);
-		return response.data;
-	} catch (error) {
-		console.error("Error fetching articles:", error);
-	}
+  try {
+    const response = await api.get(`/api/products/${article}/similar`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+  }
 };
 
 api.interceptors.response.use(
-	(response) => response,
-	(error) => {
-		console.error("API Error:", error.response || error.message);
-		return Promise.reject(error);
-	}
+  (response) => response,
+  (error) => {
+    console.error("API Error:", error.response || error.message);
+    return Promise.reject(error);
+  }
 );
 
 export default api;
