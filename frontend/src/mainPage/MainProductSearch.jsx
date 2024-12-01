ca55f14080540a48380aa865b5b1135be0d8b72b
Ray <nikanop9@yandex.ru>
2024-12-01 10:44:18 +0700

    Media fix

--- frontend/src/mainPage/MainProductSearch.jsx
+++ frontend/src/mainPage/MainProductSearch.jsx
@@ -7,66 +7,72 @@ import ContentLoader from "react-content-loader";
 import { fetchProducts } from "../API/api";
 
 const MainProductSearch = () => {
-  const [currentProduct, setCurrentProduct] = useState([]);
-  const [loadingStatus, setLoadingStatus] = useState(true);
+	const [currentProduct, setCurrentProduct] = useState([]);
+	const [loadingStatus, setLoadingStatus] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const productIds = await getAllProductIds().reverse();
-      const productsData = [];
+	useEffect(() => {
+		const fetchData = async () => {
+			const productIds = await getAllProductIds().reverse();
+			const productsData = [];
 
-      for (let temp of productIds) {
-        const pop = await fetchProducts(temp);
-        productsData.push(pop[0]);
-      }
+			for (let temp of productIds) {
+				const pop = await fetchProducts(temp);
+				productsData.push(pop[0]);
+			}
 
-      setCurrentProduct(productsData);
-      setLoadingStatus(false);
-    };
+			setCurrentProduct(productsData);
+			setLoadingStatus(false);
+		};
 
-    fetchData();
-  }, []);
+		fetchData();
+	}, []);
 
-  const gridHistory = (
-    <div className={styles.productGrid}>
-      {currentProduct.map((product, index) => (
-        <ListProductCard key={index} {...product} type="history" />
-      ))}
-    </div>
-  );
+	const gridHistory = (
+		<div className={styles.productGrid}>
+			{currentProduct.map((product, index) => (
+				<ListProductCard key={index} {...product} type="history" />
+			))}
+		</div>
+	);
 
-  const Skeleton = () => (
-    <ContentLoader
-      speed={2}
-      width={504}
-      height={430}
-      viewBox="0 0 504 430"
-      backgroundColor="#d3d3d3"
-      foregroundColor="#e0e0e0"
-      preserveAspectRatio="xMidYMid meet"
-    >
-      <rect x="0" y="0" rx="5" ry="5" width="504" height="430" />
-    </ContentLoader>
-  );
+	const Skeleton = () => (
+		<ContentLoader
+			speed={2}
+			width={504}
+			height={430}
+			viewBox="0 0 504 430"
+			backgroundColor="#d3d3d3"
+			foregroundColor="#e0e0e0"
+			preserveAspectRatio="xMidYMid meet"
+		>
+			<rect x="0" y="0" rx="5" ry="5" width="504" height="430" />
+		</ContentLoader>
+	);
 
-  const loading = (
-    <div className={styles.skeletonGrid}>
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
-    </div>
-  );
+	const loading = (
+		<div className={styles.skeletonGrid}>
+			<Skeleton />
+			<Skeleton />
+			<Skeleton />
+		</div>
+	);
 
-  const emptyHistory = <p className={styles.emptyMessage}>Нет данных</p>;
+	const emptyHistory = <p className={styles.emptyMessage}>Нет данных</p>;
 
-  return (
-    <main className={styles.container}>
-      <h1 className={styles.title}>сайтик МКЛП</h1>
-      <SearchBar />
-      <h1 className={styles.historyTitle}>История:</h1>
-      {loadingStatus ? loading : currentProduct.length > 0 ? gridHistory : emptyHistory}
-    </main>
-  );
+	return (
+		<main className={styles.container}>
+			<h1 className={styles.title}>сайтик МКЛП</h1>
+			<SearchBar />
+			<h1 className={styles.historyTitle}>История:</h1>
+			<div className>
+				{loadingStatus
+					? loading
+					: currentProduct.length > 0
+					? gridHistory
+					: emptyHistory}
+			</div>
+		</main>
+	);
 };
 
 export default MainProductSearch;
--- frontend/src/secondPage/SecondProductSearch.jsx
+++ frontend/src/secondPage/SecondProductSearch.jsx
@@ -11,117 +11,130 @@ import { ClipLoader } from "react-spinners";
 import Select from "react-select";
 
 const SecondProductSearch = () => {
-  const [products, setProducts] = useState(null);
-  const [currentProduct, setCurrentProduct] = useState([]);
-  const [loadingStatus, setLoadingStatus] = useState(true);
-  const [loadingStatusBasicProduct, setLoadingStatusBasicProduct] = useState(true);
+	const [products, setProducts] = useState(null);
+	const [currentProduct, setCurrentProduct] = useState([]);
+	const [loadingStatus, setLoadingStatus] = useState(true);
+	const [loadingStatusBasicProduct, setLoadingStatusBasicProduct] =
+		useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const lastProduct = getLastProductId();
-      if (lastProduct) {
-        const fetchedData = await fetchProducts(lastProduct);
-        setProducts(fetchedData[0]);
-        setLoadingStatusBasicProduct(false);
-      }
-    };
-    const fetchArtic = async () => {
-      const articles = getLastProductId();
-      if (articles) {
-        const fetchedData = await fetchArticles(articles);
-        for (let temp of fetchedData.articles.slice(0, 9)) {
-          const pop = await fetchProducts(temp);
-          setCurrentProduct((prevProducts) => [...prevProducts, pop[0]]);
-        }
+	useEffect(() => {
+		const fetchData = async () => {
+			const lastProduct = getLastProductId();
+			if (lastProduct) {
+				const fetchedData = await fetchProducts(lastProduct);
+				setProducts(fetchedData[0]);
+				setLoadingStatusBasicProduct(false);
+			}
+		};
+		const fetchArtic = async () => {
+			const articles = getLastProductId();
+			if (articles) {
+				const fetchedData = await fetchArticles(articles);
+				for (let temp of fetchedData.articles.slice(0, 9)) {
+					const pop = await fetchProducts(temp);
+					setCurrentProduct((prevProducts) => [...prevProducts, pop[0]]);
+				}
 
-        console.log(" data:", currentProduct);
-        setLoadingStatus(false);
-      }
-    };
-    fetchData();
-    fetchArtic();
-  }, []);
+				console.log(" data:", currentProduct);
+				setLoadingStatus(false);
+			}
+		};
+		fetchData();
+		fetchArtic();
+	}, []);
 
-  const Skeleton = () => (
-    <ContentLoader
-      speed={2}
-      width={1780}
-      height={500}
-      viewBox="0 0 1780 500"
-      backgroundColor="#d3d3d3"
-      foregroundColor="#e0e0e0"
-      preserveAspectRatio="xMidYMid meet"
-    >
-      <rect x="0" y="0" rx="5" ry="5" width="1780" height="500" />
-    </ContentLoader>
-  );
+	const Skeleton = () => (
+		<ContentLoader
+			speed={2}
+			width={1780}
+			height={500}
+			viewBox="0 0 1780 500"
+			backgroundColor="#d3d3d3"
+			foregroundColor="#e0e0e0"
+			preserveAspectRatio="xMidYMid meet"
+		>
+			<rect x="0" y="0" rx="5" ry="5" width="1780" height="500" />
+		</ContentLoader>
+	);
 
-  const grid = (
-    <div className={styles.productGrid}>
-      {currentProduct.map((currentProduct, index) => (
-        <ListProductCard key={index} {...currentProduct} />
-      ))}
-    </div>
-  );
+	const grid = (
+		<div className={styles.productGrid}>
+			{currentProduct.map((currentProduct, index) => (
+				<ListProductCard key={index} {...currentProduct} />
+			))}
+		</div>
+	);
 
-  const options = [
-    { value: "price", label: "По цене" },
-    { value: "delivery", label: "По дате" },
-    { value: "rating", label: "по рейтингу" },
-    { value: "reviews_count", label: "по отзывам" },
-  ];
+	const options = [
+		{ value: "price", label: "По цене" },
+		{ value: "delivery", label: "По дате" },
+		{ value: "rating", label: "по рейтингу" },
+		{ value: "reviews_count", label: "по отзывам" },
+	];
 
-  const handleSortChange = (selectedOption) => {
-    const sortedProducts = [...currentProduct].sort((a, b) => {
-      if (selectedOption.value === "delivery") {
-        // Сортировка по дате доставки (по возрастанию)
-        return new Date(a.delivery) - new Date(b.delivery);
-      } else if (selectedOption.value === "rating" || selectedOption.value === "reviews_count") {
-        // Сортировка по рейтингу или количеству отзывов (по убыванию)
-        return b[selectedOption.value] - a[selectedOption.value];
-      } else {
-        // Сортировка по цене (по возрастанию)
-        return a[selectedOption.value] - b[selectedOption.value];
-      }
-    });
+	const handleSortChange = (selectedOption) => {
+		const sortedProducts = [...currentProduct].sort((a, b) => {
+			if (selectedOption.value === "delivery") {
+				// Сортировка по дате доставки (по возрастанию)
+				return new Date(a.delivery) - new Date(b.delivery);
+			} else if (
+				selectedOption.value === "rating" ||
+				selectedOption.value === "reviews_count"
+			) {
+				// Сортировка по рейтингу или количеству отзывов (по убыванию)
+				return b[selectedOption.value] - a[selectedOption.value];
+			} else {
+				// Сортировка по цене (по возрастанию)
+				return a[selectedOption.value] - b[selectedOption.value];
+			}
+		});
 
-    setCurrentProduct(sortedProducts);
-  };
+		setCurrentProduct(sortedProducts);
+	};
 
-  const buttonSort = (
-    <div className={styles.buttonSortConteiner}>
-      <span className={styles.buttonSortSpan}>Сортировка</span>
-      <Select className={styles.Select} options={options} defaultValue={null} onChange={handleSortChange} />
-    </div>
-  );
+	const buttonSort = (
+		<div className={styles.buttonSortConteiner}>
+			<span className={styles.buttonSortSpan}>Сортировка</span>
+			<Select
+				className={styles.Select}
+				options={options}
+				defaultValue={null}
+				onChange={handleSortChange}
+			/>
+		</div>
+	);
 
-  return (
-    <main className={styles.container}>
-      <div className={styles.header}>
-        <Link to="/" className={styles.backButton}>
-          <button>НА ГЛАВНУЮ </button>
-        </Link>
-        <h2 className={styles.pageTitle}>Выбранный товар</h2>
-      </div>
-      {loadingStatusBasicProduct ? (
-        <div className={styles.skeletonWrapper}>
-          <Skeleton />
-        </div>
-      ) : (
-        <div className={styles.productCardContainer}>
-          <ProductCard {...products} />
-        </div>
-      )}
+	return (
+		<main className={styles.container}>
+			<div className={styles.header}>
+				<Link to="/" className={styles.backButton}>
+					<button>НА ГЛАВНУЮ </button>
+				</Link>
+				<h2 className={styles.pageTitle}>Выбранный товар</h2>
+			</div>
+			{loadingStatusBasicProduct ? (
+				<div className={styles.skeletonWrapper}>
+					<Skeleton />
+				</div>
+			) : (
+				<div className={styles.productCardContainer}>
+					<ProductCard {...products} />
+				</div>
+			)}
 
-      <section className={styles.productsSection}>
-        <div className={styles.sort}>
-          <span className={styles.analog}>Аналогичные товары</span>
-          {loadingStatus ? <ClipLoader size={60} color={"#86C232"} /> : buttonSort}
-        </div>
-        {currentProduct && currentProduct.length > 0 ? grid : ""}
-      </section>
-    </main>
-  );
+			<section className={styles.productsSection}>
+				<div className={styles.sort}>
+					<span className={styles.analog}>Аналогичные товары</span>
+					{loadingStatus ? (
+						<ClipLoader size={60} color={"#86C232"} />
+					) : (
+						buttonSort
+					)}
+				</div>
+				{currentProduct && currentProduct.length > 0 ? grid : ""}
+			</section>
+		</main>
+	);
 };
 
 export default SecondProductSearch;
